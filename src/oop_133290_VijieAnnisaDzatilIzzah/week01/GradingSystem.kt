d1017336d430a5aba0b579ea4bd2dd90e6b0efec
trytoroar <mybiggestroar@gmail.com>
2026-02-13 21:41:28 +0700

    week02: add secondary constructor

--- src/oop_133290_VijieAnnisaDzatilIzzah/week01/GradingSystem.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week01/GradingSystem.kt
@@ -1,20 +1,24 @@
 package oop_133290_VijieAnnisaDzatilIzzah.week01
 
 fun main() {
+    val name = "John Thor"
+    val score = 80
+
     val grade = when (score) {
         in 90..100 -> "A"
         in 80..89 -> "B"
         in 70..79 -> "C"
         else -> "D"
-
-        println("Status: ${calculateStatus(score)}")
-
-        val studentId: String? = null
-        val idLength = studentId?.length ?: 0
-
-        println("Panjang ID: $idLength")
     }
 
-    fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+    println("Status: ${calculateStatus(score)}")
+
+    val studentId: String? = null
+    val idLength = studentId?.length ?: 0
+
+    println("Panjang ID: $idLength")
     println("Grade kamu: $grade")
 }
+
+fun calculateStatus(score: Int) =
+    if (score > 75) "Lulus" else "Tidak Lulus"
--- src/oop_133290_VijieAnnisaDzatilIzzah/week02/Student.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week02/Student.kt
@@ -1,8 +1,21 @@
-package oop_133290_VijieAnnisaDzatilIzzah.week02
+package oop_133508_ArdifaRizkySaputra.week02
 
-class Student (val name: String, val nim: String,var major: String){
+class Student (
+    val name: String,
+    val nim: String,
+    var gpa: Double = 0.0,
+    var major: String
+) {
+
+    // Secondary Constructor (jalur umum tanpa jurusan)
+    constructor(name: String, nim: String)
+            : this(name, nim, 0.0, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
+    // Init block
     init {
-        if (nim.length != 5) {
+        if (nim.length != 6) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
