5296cbbee602ab6f4487f359849599721895085a
trytoroar <mybiggestroar@gmail.com>
2026-02-27 19:20:43 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week04
+
+fun Main {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
