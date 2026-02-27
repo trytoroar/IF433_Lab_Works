1cb10c50903b0a017092ab11661379dc06f3dc93
trytoroar <mybiggestroar@gmail.com>
2026-02-27 22:49:07 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_133290_VijieAnnisaDzatilIzzah/week04/Main.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week04/Main.kt
@@ -11,4 +11,10 @@ fun main (){
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val myElectricCar = ElectricCar("Tesla", 4, 85)
+    myElectricCar.accelerate()
+    myElectricCar.honk()
+    myElectricCar.openTrunk()
 }
