d12c68e8c0b341637d6455c211e47cbb3e6a404a
trytoroar <mybiggestroar@gmail.com>
2026-02-27 23:07:02 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_133290_VijieAnnisaDzatilIzzah/week04/Main.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week04/Main.kt
@@ -17,4 +17,16 @@ fun main (){
     myElectricCar.accelerate()
     myElectricCar.honk()
     myElectricCar.openTrunk()
+
+    println("\n--- Testing Employee Hierarchy ---")
+
+    val manager = Manager("Budi", 10_000_000)
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    println()
+
+    val developer = Developer("Sinta", 8_000_000, "Kotlin")
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
 }
