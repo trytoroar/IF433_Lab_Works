7b6d8ade79b22a813f7348a58b2991fe9ebff579
trytoroar <mybiggestroar@gmail.com>
2026-03-13 12:17:24 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week06/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} berhasil ditambahkan ke Smart Home Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
