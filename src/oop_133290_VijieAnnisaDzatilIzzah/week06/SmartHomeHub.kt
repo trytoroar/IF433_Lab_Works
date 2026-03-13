c96a60654316629a808b85ece31fb6e962d0bf1c
trytoroar <mybiggestroar@gmail.com>
2026-03-13 12:18:37 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_133290_VijieAnnisaDzatilIzzah/week06/SmartHomeHub.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week06/SmartHomeHub.kt
@@ -16,4 +16,16 @@ class SmartHomeHub {
             }
         }
     }
+
+    fun activateSecurityMode() {
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
 }
