aad4a047adce4c285dac1a1473c4e3f35960f3d6
trytoroar <mybiggestroar@gmail.com>
2026-03-27 11:27:26 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(50),
+    RARE(30),
+    EPIC(10),
+    LEGENDARY(1)
+}
