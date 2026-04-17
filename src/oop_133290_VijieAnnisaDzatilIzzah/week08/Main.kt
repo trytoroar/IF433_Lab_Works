1f2b4c7afb60bd9abf409cd1398e0cf22f8bfe83
trytoroar <mybiggestroar@gmail.com>
2026-04-17 10:54:57 +0700

    week08: implemnt java interop using !!

--- src/oop_133290_VijieAnnisaDzatilIzzah/week08/Main.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week08/Main.kt
@@ -51,4 +51,10 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
