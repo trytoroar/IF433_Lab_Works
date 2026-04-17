5f65fb7df9428bb19513e83039c0fb7939e19a40
trytoroar <mybiggestroar@gmail.com>
2026-04-17 10:11:28 +0700

    week08: create nested classed for Order

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week08/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week08
+
+class City(val name:String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?)
