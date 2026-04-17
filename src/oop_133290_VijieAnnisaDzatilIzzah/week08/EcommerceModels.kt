c81c0c39754ac2f05c9b0fa829d87c725cb32e58
trytoroar <mybiggestroar@gmail.com>
2026-04-17 11:12:00 +0700

    week08: (task) create sealed class Product and date classes

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week08
+
+sealed class Product {
+    data class Electonic(
+        val id: String,
+        val name: String,
+        val warrantyMonths: Int
+    ) : Product()
+
+    data class Clothing(
+        val id: String,
+        val name: String,
+        val size: String
+    ) : Product()
+}
