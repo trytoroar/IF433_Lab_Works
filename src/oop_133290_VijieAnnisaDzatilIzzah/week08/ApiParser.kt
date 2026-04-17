8ddd8e48e0700b2999b34d559e803edf235f1d09
trytoroar <mybiggestroar@gmail.com>
2026-04-17 11:29:42 +0700

    week08: (task) implement payment processing with !!

--- src/oop_133290_VijieAnnisaDzatilIzzah/week08/ApiParser.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week08/ApiParser.kt
@@ -24,4 +24,14 @@ class ApiParser {
             else -> null
         }
     }
+
+    fun checkout(product: Product) {
+        val productId = when (product) {
+            is Product.Electronic -> product.id
+            is Product.Clothing -> product.id
+        }
+
+        val transactionId = JavaPaymentService.processPayment(productId)!!
+        println("Transaction ID: $transactionId")
+    }
 }
