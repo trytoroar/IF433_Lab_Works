580c21e72a8454c22af0e305534f07fa853f4100
trytoroar <mybiggestroar@gmail.com>
2026-04-24 10:24:41 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
