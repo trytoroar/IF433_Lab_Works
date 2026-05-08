fd4a37f044a92db7e3390f48b7df00662ff99b28
trytoroar <mybiggestroar@gmail.com>
2026-05-08 10:21:30 +0700

    week11: create string extension functions

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
