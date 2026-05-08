6e78e1a53b86c79c714e12a034dc8b0e930b69ed
trytoroar <mybiggestroar@gmail.com>
2026-05-08 10:43:13 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
