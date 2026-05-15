0203013e49ce48e9821f35f3651a19bc93ec079b
trytoroar <mybiggestroar@gmail.com>
2026-05-15 10:30:31 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
@@ -14,5 +14,11 @@ fun dispenseKibble(
         throw DispenserJamException()
     }
 
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+
     return availableGram - requestedGram
 }
