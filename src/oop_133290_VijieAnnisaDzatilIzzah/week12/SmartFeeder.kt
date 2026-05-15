4e82653edb891edcfbcba3ea22111c0dee76468b
trytoroar <mybiggestroar@gmail.com>
2026-05-15 10:36:13 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
@@ -58,5 +58,12 @@ fun main() {
             availableGram = 1000,
             isJammed = false
         )
+    }.onSuccess { newStock ->
+
+        currentKibbleStock = newStock
+
+        println(
+            "Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr"
+        )
     }
 }
