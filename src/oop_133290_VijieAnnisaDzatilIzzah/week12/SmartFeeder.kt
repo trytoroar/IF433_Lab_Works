4b476dc85659e7d1d81d37237fbda1f2c0aeae0d
trytoroar <mybiggestroar@gmail.com>
2026-05-15 10:32:46 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
@@ -26,4 +26,25 @@ fun dispenseKibble(
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+
+    } catch (e: DispenserJamException) {
+
+        println("ERROR DISPENSER: ${e.message}")
+
+    } catch (e: FoodEmptyException) {
+
+        println("ERROR STOK: ${e.message}")
+
+    } catch (e: Exception) {
+
+        println("ERROR UMUM: ${e.message}")
+    }
 }
