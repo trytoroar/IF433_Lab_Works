f6ed194be6493eaa0afa4a66ae3e60501febbed7
trytoroar <mybiggestroar@gmail.com>
2026-05-15 10:38:26 +0700

    week12: (task) handle feeding failure using onFailure

--- src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week12/SmartFeeder.kt
@@ -65,5 +65,10 @@ fun main() {
         println(
             "Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr"
         )
+    }.onFailure { error ->
+
+        println("Peringatan ke Pemilik: ${error.message}")
+        println("(Opsional: Berikan chicken jerky secara manual)")
     }
+
 }
