b70c16a086f7d7d69e073e2513b2d0186610979c
trytoroar <mybiggestroar@gmail.com>
2026-05-22 13:53:24 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
