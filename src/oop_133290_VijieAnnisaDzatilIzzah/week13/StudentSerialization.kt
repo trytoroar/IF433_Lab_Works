bcfeb4b0dfff79ba676a593acd00dab7badf688c
trytoroar <mybiggestroar@gmail.com>
2026-05-22 13:47:57 +0700

    week13: create save and load list functions

--- src/oop_133290_VijieAnnisaDzatilIzzah/week13/StudentSerialization.kt
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week13/StudentSerialization.kt
@@ -15,3 +15,21 @@ fun fromCsv(line: String): Student {
         gpa = parts[2].toDouble()
     )
 }
+
+fun saveStudents(students: List<Student>, path: String) {
+    File(path).writeText(
+        students.joinToString("\n") { it.toCsv() }
+    )
+}
+
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path)
+            .readLines()
+            .map { fromCsv(it) }
+
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
+    }
+}
