01cdf51a3a94767ade7b171bd6f16aa65ae07d11
trytoroar <mybiggestroar@gmail.com>
2026-05-29 09:56:56 +0700

    week14: extract UserValidator and UserRepository

---
+++ src/oop_133290_VijieAnnisaDzatilIzzah/week14/SrpRefactored.kt
@@ -0,0 +1,12 @@
+package oop_133290_VijieAnnisaDzatilIzzah.week14
+
+class UserValidator {
+    fun validate(user: User): Boolean =
+        user.email.contains("@") && user.age >= 18
+}
+
+class UserRepository {
+    fun save(user: User) {
+        println("Saving user: ${user.name} to database")
+    }
+}
